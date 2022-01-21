8b9e5e8034ef05b203c177aadd57deac8b65d1c7
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-01-20 20:53:08 -0500

    Update basic structure; button click function

--- TestApp/ContentView.swift
+++ TestApp/ContentView.swift
@@ -13,18 +13,28 @@ struct Option: Hashable {
 }
 
 struct ContentView: View {
+    @State var currentOption = 0
     let options: [Option] = [
         .init(title: "Home", imageName: "House"),
-        .init(title: "Home", imageName: "House"),
-        .init(title: "Home", imageName: "House"),
-        .init(title: "Home", imageName: "House"),
+        .init(title: "About", imageName: "House"),
+        .init(title: "Other", imageName: "House"),
+        .init(title: "Stuff", imageName: "House"),
     ]
     
     var body: some View {
         NavigationView {
-            ListView(options: options)
+            ListView(
+                options: options,
+                currentSelection: $currentOption
+            )
             
-            MainView()
+            switch currentOption {
+            case 1:
+                Text("Hello world")
+            default:
+                MainView()
+                
+            }
         }
         .frame(minWidth: 600, minHeight: 400)
     }
@@ -32,33 +42,71 @@ struct ContentView: View {
 
 
 struct MainView: View {
+    let cols: [GridItem] = [
+        .init(.flexible()),
+        .init(.flexible()),
+        .init(.flexible()),
+        .init(.flexible())
+    ]
+    
+    let videosImages = Array(1...6).map { "sunflower\($0)" }
+    
     var body: some View {
-        Text("List")
+        VStack {
+            Image("download")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            LazyVGrid(columns: cols) {
+                ForEach(videosImages, id: \.self) { imageName in
+                    VStack {
+                        Image(imageName)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                        Text("Video Title Goes Here")
+                            .bold()
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ListView: View {
     let options: [Option]
-    
+    @Binding var currentSelection: Int
     var body: some View {
         VStack {
-            ForEach(options, id: \.self) {
-                option in HStack {
+            let current = options[currentSelection]
+            ForEach(options, id: \.self) { option in
+                HStack {
                     Image(systemName: option.imageName)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 20)
                     
                     Text(option.title)
-                    
+                        .foregroundColor(current == option ?
+                                         Color.blue : Color.white
+                        )
                     Spacer()
                 }
                 .padding()
+                .onTapGesture {
+                    if currentSelection == 1 {
+                        currentSelection = 0
+                    } else {
+                        self.currentSelection = 1
+                    }
+                }
+                
+                
             }
-            Spacer()
+            
         }
     }
 }
+
 
 
 struct ContentView_Previews:PreviewProvider {
